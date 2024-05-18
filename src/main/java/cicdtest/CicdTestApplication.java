902c3628ed0268fc8547a32146bd00f32c089306
tayadepss <148903377+tayadepss@users.noreply.github.com>
2024-05-18 20:04:50 +0530

    Update CicdTestApplication.java

--- src/main/java/cicdtest/CicdTestApplication.java
+++ src/main/java/cicdtest/CicdTestApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController("/")
 public class CicdTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdTestApplication.class, args);
+	}
+	@GetMapping
+	public String getMsg() {
+		return "Hi.......";
 	}
 
 }
